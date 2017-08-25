6b792f870ae9b0bd5f198445888f4dd794c081ad
Adam Kimball <ad00m@Adams-MacBook-Pro.local>
2017-08-25 09:08:54 -0600

    images show up when clicked

--- javascript/main.js
+++ javascript/main.js
@@ -2,20 +2,8 @@ $(document).ready( function() {
 
   $('.matches').hide();
 
-  $('.r1c1').on('click', function(){
-        $("#fish1").fadeIn();
-  });
-
-  $('.r3c2').on('click', function(){
-        $("#fish2").fadeIn();
-  });
-
-  $('.r1c2').on('click', function(){
-        $("#bear1").fadeIn();
-  });
-
-  $('.r4c4').on('click', function(){
-        $("#bear2").fadeIn();
+  $('.card').one('click', function() {
+    $(this).find(':nth-child(2)').fadeIn();
   });
 
 });
